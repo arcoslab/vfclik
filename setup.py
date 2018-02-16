c7b35d3c43fc2692fcffae1a22415d0c151b9f9d
daniel garcia vaglio <degv364@gmail.com>
2018-02-16 09:36:41 -0600

    Make setup pep8 compliant

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@
 from distutils.core import setup
 
 
-setup(name='vfclik',
-      version='0.1',
-      description='Vector field closed loop inverse kinematics',
-      author='Federico Ruiz Ugalde',
-      author_email='memeruiz@gmail.com',
-      url='http://www.arcoslab.org/',
-      package_dir={'vfclik': 'src'},
-      packages=['vfclik'],
-      scripts=['scripts/vfclik', 'scripts/bridge', 'scripts/debug_jointlimits', 'scripts/joint_p_controller', 'scripts/monitor_distance', 'scripts/nullspace', 'scripts/object_feeder', 'scripts/vf', 'scripts/vfclik']
-     )
+setup(
+    name='vfclik',
+    version='0.1',
+    description='Vector field closed loop inverse kinematics',
+    author='Federico Ruiz Ugalde',
+    author_email='memeruiz@gmail.com',
+    url='http://www.arcoslab.org/',
+    package_dir={'vfclik': 'src'},
+    packages=['vfclik'],
+    scripts=['scripts/vfclik', 'scripts/bridge',
+             'scripts/debug_jointlimits',
+             'scripts/joint_p_controller',
+             'scripts/monitor_distance', 'scripts/nullspace',
+             'scripts/object_feeder', 'scripts/vf', 'scripts/vfclik']
+)
